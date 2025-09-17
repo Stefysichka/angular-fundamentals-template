b8529c938ff6a002cac4e8d7973651a857a84225
Stefysichka <stefaniamavdruk@gmail.com>
2025-09-18 01:41:19 +0300

    Fixed problems

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,13 +4,14 @@ import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { SharedModule } from '@shared/shared.module';
 import { AppComponent } from '@app/app.component';
 import { CourseInfoComponent } from '@features/course-info/course-info.component';
+import { CoursesListComponent } from '@features/courses/courses-list/courses-list.component';
 import { NotAuthorizedGuard } from '@app/auth/guards/not-authorized.guard';
 import { AuthorizedGuard } from '@app/auth/guards/authorized.guard';
 import { CoursesStoreService } from '@app/services/courses-store.service';
 import { CoursesService } from '@app/services/courses.service';
 
 @NgModule({
-  declarations: [AppComponent, CourseInfoComponent],
+  declarations: [AppComponent, CourseInfoComponent, CoursesListComponent],
   imports: [
     BrowserModule,
     SharedModule,
--- src/app/features/courses/courses-list/courses-list.component.ts
+++ src/app/features/courses/courses-list/courses-list.component.ts
@@ -1,13 +1,22 @@
 import { Component, Input, Output, EventEmitter } from '@angular/core';
 import { mockedAuthorsList } from '../../../shared/mocks/mocks';
 
+export interface Course {
+  id: string;
+  title: string;
+  description: string;
+  creationDate: Date;
+  duration: number;
+  authors: string[];
+}
+
 @Component({
   selector: 'app-courses-list',
   templateUrl: './courses-list.component.html',
-  styleUrls: ['./courses-list.component.scss']
+  styleUrls: ['./courses-list.component.css']
 })
 export class CoursesListComponent {
-  @Input() courses: any[] = [];
+  @Input() courses: Course[] = [];
   @Input() editable: boolean = false;
 
   @Output() showCourse = new EventEmitter<string>();
@@ -31,4 +40,8 @@ export class CoursesListComponent {
   onDelete(id: string) {
     this.deleteCourse.emit(id);
   }
+  ngOnInit() {
+  console.log('CoursesListComponent ngOnInit courses:', this.courses);
+}
+
 }
