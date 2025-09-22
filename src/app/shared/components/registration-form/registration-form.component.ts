fa728277b96e6e105d322480adcd92bc1e19d809
Stefysichka <stefaniamavdruk@gmail.com>
2025-09-22 22:50:03 +0300

    add test selectors and minimum validation for email and password

--- src/app/shared/components/registration-form/registration-form.component.ts
+++ src/app/shared/components/registration-form/registration-form.component.ts
@@ -20,8 +20,8 @@ export class RegistrationFormComponent implements OnInit {
   ngOnInit(): void {
     this.registrationForm = this.fb.group({
       name: ['', [Validators.required, Validators.minLength(6)]],
-      email: ['', [Validators.required]],
-      password: ['', [Validators.required]],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
     });
   }
 
