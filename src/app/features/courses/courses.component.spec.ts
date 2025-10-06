f3d5e8d75ddc27a4b9bdaa55b4e4c50138e1e798
Stefysichka <stefaniamavdruk@gmail.com>
2025-10-06 12:57:24 +0300

    autocode 2

--- src/app/features/courses/courses.component.spec.ts
+++ src/app/features/courses/courses.component.spec.ts
@@ -1,20 +1,34 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { HttpClientTestingModule } from '@angular/common/http/testing';
-import { TestBed } from '@angular/core/testing';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+
 import { CoursesComponent } from './courses.component';
+import { CoursesStateFacade } from '@app/store/courses/courses.facade';
 import { CoursesService } from '../../services/courses.service';
+import { reducers, effects } from '@app/store';
 
 describe('CoursesComponent', () => {
+  let component: CoursesComponent;
+  let fixture: ComponentFixture<CoursesComponent>;
+
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [CoursesComponent],
-      imports: [HttpClientTestingModule], 
-      providers: [CoursesService],      
+      imports: [
+        HttpClientTestingModule, 
+        StoreModule.forRoot(reducers), 
+        EffectsModule.forRoot(effects), 
+      ],
+      providers: [CoursesStateFacade, CoursesService],
     }).compileComponents();
+
+    fixture = TestBed.createComponent(CoursesComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
   });
 
   it('should create', () => {
-    const fixture = TestBed.createComponent(CoursesComponent);
-    const component = fixture.componentInstance;
     expect(component).toBeTruthy();
   });
 });
